0a6832b8c6f0f823af774fc0c6e9369bfad4e57d
Administrator <958117346@qq.com>
2018-10-22 15:01:53 +0800

    命令行参数练习

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -1,28 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+a = sys.argv[1:]
+print(a)
 
 
-def main():
-    income = int(sys.argv[1])
-    value = income - 3500
-    if value <= 0:
-        result = 0
-    elif 0 < value <= 1500:
-        result = value * 0.03 - 0
-    elif 1500 < value <= 4500:
-        result = value * 0.1 - 105
-    elif 4500 < value <= 9000:
-        result = value * 0.2 - 555
-    elif 9000 < value <= 35000:
-        result = value * 0.25 - 1005
-    elif 35000 < value <= 55000:
-        result = value * 0.3 - 2755
-    elif 55000 < value <= 80000:
-        result = value * 0.35 - 5505
-    else:
-        result = value * 0.45 - 13505
-    print('{:.2f}'.format(result))
-
-
-if __name__ == '__main__':
-    main()
